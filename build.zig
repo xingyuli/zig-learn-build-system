b0d0b86acddcc652c45e4b7be17fcc9aa532b0d0
Vic Lau <xingyulliiuu@gmail.com>
2025-04-05 23:12:56 +0800

    Adding a Convenience Step for Running the Application

--- build.zig
+++ build.zig
@@ -13,4 +13,9 @@ pub fn build(b: *std.Build) void {
     });
 
     b.installArtifact(exe);
+
+    const run_exe = b.addRunArtifact(exe);
+
+    const run_step = b.step("run", "Run the application");
+    run_step.dependOn(&run_exe.step);
 }
